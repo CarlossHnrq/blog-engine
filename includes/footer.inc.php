59ffac3894610dd2a700edb0a3100a6837b46d15
Carlos Henrique <carlos_hnrq@hotmail.com>
2019-04-01 14:54:01 -0300

    Make footer and add includes

---
+++ includes/footer.inc.php
@@ -0,0 +1,4 @@
+	<footer class="footer">
+		<p>Copyright © 2019 Blog-engine Inc.</p>
+	</footer>
+</body>
---
+++ includes/head.inc.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Baloo+Chettan" rel="stylesheet">
+		<title>Blog-Engine</title>
+	</head>
+	<body>
