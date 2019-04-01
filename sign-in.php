a88a473d1713b67404f821d60549866e0ef1c56a
Carlos Henrique <carlos_hnrq@hotmail.com>
2019-04-01 17:45:11 -0300

    create sign-in page 1

---
+++ sign-in.php
@@ -0,0 +1,40 @@
+<?php include_once 'includes/head.inc.php' ?>
+<?php include_once 'includes/header.inc.php' ?>
+
+<section>
+	<div class="form-background">
+		<div class="info-form-sign-in">
+			<div>
+				<h2>Discover</h2>
+				<p>a wealth of genuine, unique information</p>
+			</div>
+			<div>
+				<h2>Create</h2>
+				<p>original, in-depht, usefull, media-rich articles</p>
+			</div>
+			<div>
+				<h2>Connect</h2>
+				<p>with a passionate community of writers and enthusiasts</p>
+			</div>
+			<a href="sing-up.php">Sign up now!</a>
+		</div>
+		<div>
+			<form class="form-sign-in">
+					<div>
+						<input type="text" name="username" placeholder="Username">
+					</div>
+					<div>
+						<input type="text" name="password" placeholder="Password">
+					</div>
+					<div>
+						<button><a href="/">Sign in</a></button>
+					</div>
+					<div>
+						<a href="/" class="">Sign up |</a><a href="/">Can't access your account?</a>
+					</div>
+			</form>
+		</div>
+	</div>
+</section>
+
+<?php include_once 'includes/footer.inc.php' ?>
