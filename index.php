a07e9936bafdae2318b459917022dd586a68d0bc
Carlos Henrique <carlos_hnrq@hotmail.com>
2019-04-01 17:44:29 -0300

    add nav to index

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
 <?php include_once 'includes/head.inc.php' ?>
 <?php include_once 'includes/header.inc.php' ?>
 
-	
-
+		<nav class="nav-bg">
+			<div class="menu-nav">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Film</a></li>
+					<li><a href="#">Sports</a></li>
+					<li><a href="#">Marketing</a></li>
+					<li><a href="#">Economy</a></li>
+					<li><a href="#">Programming</a></li>
+					<li><a href="#">Science</a></li>
+					<li><a href="#">Health</a></li>
+					<li><a href="#">History</a></li>
+					<li><a href="#">Politics</a></li>
+					<li><a href="#">Religion</a></li>
+					<li><a href="#">World</a></li>
+					<li><a href="#">More></a></li>
+				</ul>
+			</div>
+		</nav>
+		
 <?php include_once 'includes/footer.inc.php' ?>
 
