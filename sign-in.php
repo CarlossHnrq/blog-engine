5cfd6be09fd45d16f3dda6087c4a32836f7f6459
Carlos Henrique <carlos_hnrq@hotmail.com>
2019-04-02 15:51:23 -0300

    create sign up form

--- sign-in.php
+++ sign-in.php
@@ -1,42 +1,43 @@
 <?php include_once 'includes/head.inc.php' ?>
 <?php include_once 'includes/header.inc.php' ?>
-
-<section>
-	<div class="form-background">
-		<div class="info-form-sign-in">
-			<div class="separate-info">
-				<h2>Discover</h2>
-				<p>a wealth of genuine, unique information</p>
+<div class="sign-in-background">
+	<section>
+		<div class="form-background">
+			<div class="info-form-sign-in">
+				<div class="separate-info">
+					<h2>Discover</h2>
+					<p>a wealth of genuine, unique information</p>
+				</div>
+				<div class="separate-info">
+					<h2>Create</h2>
+					<p>original, in-depht, usefull, media-rich articles</p>
+				</div>
+				<div class="separate-info">
+					<h2>Connect</h2>
+					<p>with a passionate community of writers and enthusiasts</p>
+				</div>
+				<div class="separate-info">
+					<a href="sign-up.php">Sign up now!</a>
+				</div>
 			</div>
-			<div class="separate-info">
-				<h2>Create</h2>
-				<p>original, in-depht, usefull, media-rich articles</p>
-			</div>
-			<div class="separate-info">
-				<h2>Connect</h2>
-				<p>with a passionate community of writers and enthusiasts</p>
-			</div>
-			<div class="separate-info">
-				<a href="sing-up.php">Sign up now!</a>
+			<div>
+				<form class="form-sign-in">
+						<div class="separate-sign-in">
+							<input type="text" name="username" placeholder="Username">
+						</div>
+						<div class="separate-sign-in">
+							<input type="text" name="password" placeholder="Password">
+						</div>
+						<div class="separate-sign-in">
+							<a href="/" class="botao-sign-in">Sign in</a>
+						</div>
+						<div class="separate-sign-in">
+							<a href="sign-up.php" class="link-sign-in">Sign up</a> | <a href="/" class="link-sign-in">Can't access your account?</a>
+						</div>
+				</form>
 			</div>
 		</div>
-		<div>
-			<form class="form-sign-in">
-					<div class="separate-sign-in">
-						<input type="text" name="username" placeholder="Username">
-					</div>
-					<div class="separate-sign-in">
-						<input type="text" name="password" placeholder="Password">
-					</div>
-					<div class="separate-sign-in">
-						<a href="/" class="botao-sign-in">Sign in</a>
-					</div>
-					<div class="separate-sign-in">
-						<a href="/" class="link-sign-in">Sign up</a> | <a href="/" class="link-sign-in">Can't access your account?</a>
-					</div>
-			</form>
-		</div>
-	</div>
-</section>
+	</section>
+</div>
 
 <?php include_once 'includes/footer.inc.php' ?>
---
+++ sign-up.php
@@ -0,0 +1,26 @@
+<?php include_once 'includes/head.inc.php' ?>
+<?php include_once 'includes/header.inc.php' ?>
+
+<div class="sign-up-background">
+	<div class="sign-up-form-background">
+		<section>
+			<form>
+				<div class="sign-up-form">
+					<label>Your email adress</label>
+					<input type="text" name="email">
+					<label>Choose a username</label>
+					<input type="text" name="username">
+					<label>Choose a password</label>
+					<input type="text" name="password">
+					<p>By creating your account via any of the options below, you agree to our <a href="/">terms of service</a>.</p>
+					<button>Create your account</button>
+				</div>
+				<div class="back-to-sign-in">
+					<a href="sign-in.php">Already have a blog engine account?</a>
+				</div>
+			</form>
+		</section>
+	</div>
+</div>
+
+<?php include_once 'includes/footer.inc.php' ?>
